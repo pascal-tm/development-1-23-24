700ef3fc8481b71b5bc1bf6fc2dc23eaa881e86d
Pascalculator <info@pascalculator.be>
2024-03-04 12:48:46 +0100

    add additional exercise explanation

--- js/loops.js
+++ js/loops.js
@@ -18,6 +18,28 @@ for( let table = 1; table < 10; ++table )
     } 
 }
 
+// Exercise
+// Goal: print the tables of multiplications from 1 tot 9 to the console in the following format:
+// the table of 1: 1 - 2 - 3 - 4 - ... - 9
+// the table of 2: 1 - 4 - 6 - 8 - ... - 18
+// ...
+// the table of 9: 9 - 27 - 36 - 45 - ... - 81
+// FORMAT: the table of (table): (multiplication) - (multiplication)
+
+// You have to use nested for loops to do this
+// Create an array that will contain the multiplications of a specific table
+// Then you print the result for every table and it is formated like this: the table of 2: 1 - 4 - 6 - 8
+
+// Tip: start with the outer for loop. Try to print out the following:
+// the table of 1:
+// the table of 2:
+// ...
+// the table of 9:
+// If that works, you can add the nested for-loop
+
+
+
+
 // foreach
 
 // while
