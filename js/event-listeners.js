90caec3c14fe592785cdbf512e544fc2570b5081
Pascalculator <info@pascalculator.be>
2024-04-15 11:27:34 +0200

    add additional event listener exercises

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -12,20 +12,20 @@ document.getElementById("input01").addEventListener("keyup", (event) => {
   });
 
 // Let's make the DOM-manipulation exercise a bit more dynamic:
-let guests = ["Luis", "Laura", "Jha"];
+let guestlist = ["Luis", "Laura", "Jha"];
 let bouncerParagraph = document.getElementById("bouncer");
 
-document.getElementById("guestList").addEventListener("keyup", (event) => {
+document.getElementById("guest").addEventListener("keyup", (event) => {
 
-    let visitor = document.getElementById("guestList").value;
+    let guest = document.getElementById("guest").value;
 
     if (event.isComposing || event.keyCode === 229) {
       return;
     }
 
-    let visitorIsAllowed = guests.includes(visitor);
+    let guestIsAllowed = guestlist.includes(guest);
 
-    if (visitorIsAllowed) {
+    if (guestIsAllowed) {
         bouncerParagraph.classList.remove('banned');
         bouncerParagraph.classList.add('alert', 'allowed');
     } else {
@@ -50,6 +50,15 @@ document.querySelector('#paragraph01 span').addEventListener("click", (event) =>
 // Exercise
 // Create a button (you can use a regular span element that contains the word 'add', ie. <span>add</span>)
 // add an event listener to that button that listens to the click event
-// When you click on the 'add'-button a list item (<li></li>) is added to an empty unordered list (<ul></ul>) that is located below the 'add'-button
+// When you click on the 'add'-button a list item (<li>New item (<span>x</span>)</li>) is added to an empty unordered list (<ul></ul>) that is located below the 'add'-button and which will function  as your container.
+// The newly added items also have a remove button (ie. <span>x</span>). When you click on that button, the list item is removed.
+
+// Additional exercise
+// Create an array called guestlist that contains some names
+// When opening the document, these name are shown in a <ul></ul> element by adding them as list-items (<li></li>)
+// Above the ul-element, add an input type text field with a button 'add' next to it.
+// When you open the document, all the names in the guestlist array are shown in the ul-element.
+// When you write a name in the input field, the name is added to the array and to the ul-element
+// You should be able to remove any name from the ul-element (make sure to also delete that name from the guestlist array)
 
   
